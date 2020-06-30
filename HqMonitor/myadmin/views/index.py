e1480e6fe4a3ede35c214e47781545f0a288b243
wangqcl <wangqcl@163.com>
2020-06-30 18:33:14 +0800

    用户密码加密rsa

--- HqMonitor/myadmin/views/index.py
+++ HqMonitor/myadmin/views/index.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.hashers import check_password
 
-from common.models import Users,Compinfo
-import time,json,datetime
+from common.models import Users, Compinfo
+import datetime
+import base64
+from Crypto import Random
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
+from Crypto.PublicKey import RSA
+
 
 # 后台首页
 def index(request):
@@ -13,15 +18,17 @@ def index(request):
     us_count = Users.objects.exclude(state=3).all().count()
     co_count = Compinfo.objects.exclude(state=3).all().count()
     context = {
-        "us_count":us_count,
-        "co_count":co_count
+        "us_count": us_count,
+        "co_count": co_count
     }
-    return render(request,"myadmin/index.html",context)
+    return render(request, "myadmin/index.html", context)
+
 
 # ==============后台管理员操作====================
 # 会员登录表单
 def login(request):
-    return render(request,'myadmin/login.html')
+    return render(request, 'myadmin/login.html')
+
 
 # 会员执行登录
 def dologin(request):
@@ -29,55 +36,67 @@ def dologin(request):
     verifycode = request.session['verifycode']
     code = request.POST['code'].upper()
     if verifycode != code:
-        context = {'info': '验证码错误！'}
-        return render(request, "myadmin/login.html", context)
+        context = {'info': '验证码错误！', 'msg': 'fail'}
+        return JsonResponse(context)
     # 判断是否有特殊字符!@_
     user_name = request.POST['username']
     string = "~#$%^&*()+-*/<>,.[]\/"
     for i in string:
         if i in user_name:
-            context = {'info': '您的输入包含特殊字符,请重新输入！'}
-            return render(request, "myadmin/login.html", context)
+            context = {'info': '您的输入包含特殊字符,请重新输入！', 'msg': 'fail'}
+            return JsonResponse(context)
 
     try:
         user = Users.objects.get(username=user_name)
         passw = request.POST['password']
+        passw = passw.encode('utf8')
+        RANDOM_GENERATOR = Random.new().read
+        with open('web/encryption/master-private.pem') as f:
+            key = f.read()
+        rsakey = RSA.importKey(key.encode('utf8'))
+        cipher = Cipher_pkcs1_v1_5.new(rsakey)
+        password = cipher.decrypt(base64.b64decode(passw), RANDOM_GENERATOR)
         if user.state == 0:
             if int((datetime.datetime.now() - user.login_time).total_seconds()) < 300:
-                context = {'info': '账号锁定5分钟内不能登陆!'}
-                return render(request, "myadmin/login.html", context)
+                context = {'info': '账号锁定5分钟内不能登陆!', 'msg': 'fail'}
+                return JsonResponse(context)
             if user.count > 4:
                 user.login_time = datetime.datetime.now()
                 user.count = 0
                 user.save()
-                context = {'info': '密码输入超过5次，用户锁定5分钟'}
-                return render(request, "myadmin/login.html", context)
-            ps_bool = check_password(passw,user.password)
+                context = {'info': '密码输入超过5次，用户锁定5分钟', 'msg': 'fail'}
+                return JsonResponse(context)
+            ps_bool = check_password(password, user.password)
             if ps_bool == True:
                 user.count = 0
                 user.save()
                 request.session['adminuser'] = user.username
-                return redirect(reverse('myadmin_index'))
-            else:
-                user.count += 1
-                user.save()
-                context = {'info': '登录账号/密码错误！'}
+                context = {'info': '登陆成功', 'msg': 'success', 'url': '/myadmin'}
+                return JsonResponse(context)
+
+            user.count += 1
+            user.save()
+            context = {'info': '登录账号/密码错误！', 'msg': 'fail'}
+            return JsonResponse(context)
         else:
-            context = {'info': '无登录权限！'}
+            context = {'info': '无登录权限！', 'msg': 'fail'}
+            return JsonResponse(context)
     except:
-        context = {'info': '用户名不存在！'}
-    return render(request, "myadmin/login.html", context)
+        context = {'info': '用户名不存在！', 'msg': 'fail'}
+        return JsonResponse(context)
+
 
 # 会员退出
 def logout(request):
     del request.session['adminuser']
     return redirect(reverse('myadmin_login'))
 
+
 # 会员登录表单
 def verify(request):
     import random
     from PIL import Image, ImageDraw, ImageFont
-    bgcolor = (242,164,247)
+    bgcolor = (242, 164, 247)
     width = 100
     height = 25
     im = Image.new('RGB', (width, height), bgcolor)
--- HqMonitor/web/views/center.py
+++ HqMonitor/web/views/center.py
@@ -1,33 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
-from common.models import Users,Compinfo
+from django.http import HttpResponse, JsonResponse
+from common.models import Users, Compinfo
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.hashers import check_password
-from . import user_edit
+from . import user_edit, Encryption
 import datetime
+import base64
+from Crypto import Random
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
+from Crypto.PublicKey import RSA
 
 
 # Create your views here.
 
 def center(request):
     '''跳转用户个人中心'''
-    log_user = request.session['webuser'] #获取当前登录账号
+    print(111111111111111)
+    log_user = request.session['webuser']  # 获取当前登录账号
     userinfo = Users.objects.get(username=log_user)
-    if userinfo.state == 0:#判断是否超级管理员
+    if userinfo.state == 0:  # 判断是否超级管理员
         comp_list = Compinfo.objects.exclude(state=2).all()
     else:
         comp_list = userinfo.compinfo_set.all()
 
     content = {
-        'user':userinfo,
-        'userinfo':userinfo,
-        'comp_list':comp_list
+        'userinfo': userinfo,
+        'comp_list': comp_list
     }
-    return render(request, "web/monweb/Center.html",content)
+    return render(request, "web/monweb/Center.html", content)
 
 
-#执行编辑
+# 执行编辑
 @user_edit
 def edit(request):
     try:
@@ -47,6 +51,7 @@ def edit(request):
 # ==============后台管理员操作====================
 # 会员登录表单
 def login(request):
+    Encryption(request)
     return render(request, 'web/monweb/login.html')
 
 
@@ -57,47 +62,54 @@ def dologin(request):
     code = request.POST['code']
     code = code.upper()
     if verifycode != code:
-        context = {'info': '验证码错误！'}
-        return render(request, "web/monweb/login.html", context)
+        context = {'info': '验证码错误！', 'msg': 'fail'}
+        return JsonResponse(context)
     # 判断是否有特殊字符
     user_name = request.POST['username']
     string = "~#$%^&*()+-*/<>,.[]\/"  # 去除特殊字符的正则
     for i in string:
         if i in user_name:
-            context = {'info': '您的输入包含特殊字符,请重新输入！'}
-            return render(request, "web/monweb/login.html", context)
+            context = {'info': '您的输入包含特殊字符,请重新输入！', 'msg': 'fail'}
+            return JsonResponse(context)
     try:
         # 根据账号获取登录者信息
         user = Users.objects.get(username=user_name)
         passw = request.POST['password']
+        passw = passw.encode('utf8')
+        RANDOM_GENERATOR = Random.new().read
+        with open('web/encryption/master-private.pem') as f:
+            key = f.read()
+        rsakey = RSA.importKey(key.encode('utf8'))
+        cipher = Cipher_pkcs1_v1_5.new(rsakey)
+        password = cipher.decrypt(base64.b64decode(passw), RANDOM_GENERATOR)
         # 判断是否禁用
         if user.state != 2:
             if int((datetime.datetime.now() - user.login_time).total_seconds()) < 300:
-                context = {'info': '账号锁定5分钟内不能登陆!'}
-                return render(request, "web/monweb/login.html", context)
+                context = {'info': '账号锁定5分钟内不能登陆!', 'msg': 'fail'}
+                return JsonResponse(context)
             if user.count > 4:
                 user.login_time = datetime.datetime.now()
                 user.count = 0
                 user.save()
-                context = {'info': '密码输入超过5次，用户锁定5分钟'}
-                return render(request, "web/monweb/login.html", context)
-            ps_bool = check_password(passw, user.password)
+                context = {'info': '密码输入超过5次，用户锁定5分钟', 'msg': 'fail'}
+                return JsonResponse(context)
+            ps_bool = check_password(password, user.password)
             if ps_bool == True:
                 request.session['webuser'] = user.username
                 user.count = 0
                 user.save()
-                return redirect(reverse('web_center'))
+                context = {'info': '登陆成功', 'msg': 'success', 'url': '/web/center'}
+                return JsonResponse(context)
             user.count += 1
             user.save()
-            context = {'info': '登录账号/密码错误！'}
-            return render(request, "web/monweb/login.html", context)
+            context = {'info': '登录账号/密码错误！', 'msg': 'fail'}
+            return JsonResponse(context)
         else:
-            context = {'info': '无登录权限！'}
-            return render(request, "web/monweb/login.html", context)
+            context = {'info': '无登录权限！', 'msg': 'fail'}
+            return JsonResponse(context)
     except:
-
-        context = {'info': '用户名不存在！'}
-    return render(request, "web/monweb/login.html", context)
+        context = {'info': '用户名不存在！', 'msg': 'fail'}
+        return JsonResponse(context)
 
 
 # 会员退出
@@ -108,10 +120,10 @@ def logout(request):
 
 # 会员登录表单
 def verify(request):
-    #引入随机函数模块
+    # 引入随机函数模块
     import random
     from PIL import Image, ImageDraw, ImageFont
-    bgcolor = (242,164,247)
+    bgcolor = (242, 164, 247)
     width = 100
     height = 25
     im = Image.new('RGB', (width, height), bgcolor)
